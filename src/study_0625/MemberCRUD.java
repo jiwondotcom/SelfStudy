f28973aa788554765f4ac52b6735299fc64b72fd
jiwon <xxxlikeag1_@naver.com>
2021-06-25 17:52:10 +0900

    Update MemberCRUD.java

--- src/study_0625/MemberCRUD.java
+++ src/study_0625/MemberCRUD.java
@@ -17,7 +17,7 @@ public class MemberCRUD {
 
 	
 	
-	// 1. SELECT
+	// 1. SELECT - 회원 정보
 	public static ArrayList<Member> getMemberList(Connection con) {
 		ArrayList<Member> list = new ArrayList<Member>();
 		Statement stmt = null;
@@ -27,7 +27,7 @@ public class MemberCRUD {
 
 		try {
 			stmt = con.createStatement();
-			// MEMBER 테이블에서 membercode 오름차순으로 가져온다. sql에 대입
+			// MEMBER 테이블에서 memberCode 오름차순으로 가져온다. sql에 대입
 			rs = stmt.executeQuery(sql);
 			while (rs.next()) {
 				list.add(new Member(rs.getInt(1), rs.getString(2), rs.getString(3),
@@ -41,7 +41,7 @@ public class MemberCRUD {
 	
 	
 	
-	// 2. INSERT
+	// 2. INSERT - 회원 추가
 	public Member insertMember(Connection con, Member member) {
 		
 		PreparedStatement pstmt = null;
@@ -73,23 +73,51 @@ public class MemberCRUD {
 	
 	// 3. UPDATE - 회원정보 수정
 	public void updateMember(Connection con, Member member) { 
+
+		PreparedStatement pstmt = null;
+		
+		String updateSql = "UPDATE MEMBER SET ID = ?, PASSWORD = ?, MNAME = ?, AGE = ?, PHONENUM = ?, EMAIL = ?"
+				+ "WHERE MEMBERCODE = ?";
+		// 멤버코드에 따라 회원정보를 각각 정보를 업데이트한다. (수정한다.)
+		
+		try {
+			pstmt = con.prepareStatement(updateSql);
+			
+			pstmt.setString(1, member.getId());
+			pstmt.setString(2, member.getPassWord());
+			pstmt.setString(3, member.getmName());
+			pstmt.setInt(4, member.getAge());
+			pstmt.setString(5, member.getPhoneNum());
+			pstmt.setString(6, member.getEmail());
+			pstmt.setInt(7, member.getMemberCode());
+			
+			pstmt.executeUpdate();
+	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	// 4. DELETE - 회원 삭제
+	public void deleteMember (Connection con, Member member) {
+		
+		PreparedStatement pstmt = null;
+		
+		String deleteSql = "DELETE FROM MEMBER WHERE MEMBERCODE = ?";
+		
+		try {
+			pstmt = con.prepareStatement(deleteSql);
+			
+			pstmt.setInt(1, member.getMemberCode());
+			// memberCode가 해당되는 MEMEBER 테이블 내에 데이터를 삭제한다.
+		
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	
 	}
+	
+}
