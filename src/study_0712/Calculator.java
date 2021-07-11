6b96e79a2875649d7ee7aef854f9abdeca1ee078
jiwon <xxxlikeag1_@naver.com>
2021-07-12 00:56:40 +0900

    commit

---
+++ src/study_0712/Calculator.java
@@ -0,0 +1,9 @@
+package study_0712;
+
+interface Calculator {
+	long add(long n1, long n2);
+	long substract(long n1, long n2 );
+	long multiply(long n1, long n2 );
+	double divide(double n1, double n2 );
+}
+
